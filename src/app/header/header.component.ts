3a868246a63e5f6da742e05e67e0fe87fb6b2227
MissCarolin <carolin.wagner.hh@gmx.de>
2021-01-25 15:42:56 +0100

    added navigation of Recipe and List Items

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     collapsed = true;
+    @Output() featureSelected = new EventEmitter<string>();
 
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    onSelect(feature: string) {
+      this.featureSelected.emit(feature);
+    }
 
 }
